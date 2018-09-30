a78c643fca93e933c5d7d3449a675c230849c59b
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2018-09-30 22:36:24 +0330

    Initialize repository with simple Spring boot 2 project (Spring Initializr)

---
+++ src/main/java/com/github/rezaep/springmonitoring/Application.java
@@ -0,0 +1,12 @@
+package com.github.rezaep.springmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
